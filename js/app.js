84f3b45e9d378096cea5bad491b8a3f6cfc5264e
MarvNet <marvin.kai@gmx.de>
2017-10-31 14:11:16 +0100

    Added basic Location structure

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-'use strict';
 
-var locations = [
+locations = [
 	{
 		title: 'MyZeil (Shopping Center)',
 		lat: 50.114354,
 		long: 8.681447
 	}
 ];
+
+var Location = function(data) {
+	var self = this;
+	this.latitude = data.lat;
+	this.longitude = data.long;
+	this.url = "";
+	this.street = "";
+	this.city = "";
+	this.phone = "";
+
+	this.isVisible = ko.observable(true);
+}
+
