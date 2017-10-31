9a13201a71057bb3a1a22e40617fce4bd735bdd7
MarvNet <marvin.kai@gmx.de>
2017-10-31 18:28:26 +0100

    Added places

--- js/markers.js
+++ js/markers.js
@@ -10,6 +10,26 @@ var mapLocations = [
 		lat: 50.117534,
 		long: 8.652028
 		// Senckenberg Museum
+	},
+	{
+		lat: 50.110372,
+		long: 8.648573
+		// Messe
+	},
+	{
+		lat: 50.109367,
+		long: 8.652607
+		// Skyline Plaza
+	},
+	{
+		lat: 50.116357,
+		long: 8.699471
+		// Zoo Frankfurt
+	},
+	{
+		lat: 50.088978,
+		long: 8.711305
+		// Goetheturm
 	}
 ];
 
