7a6ae28ab791635a4805a1620ab0b9aa34a03a05
MarvNet <marvin.kai@gmx.de>
2017-10-31 18:01:52 +0100

    Bugfix: JSON

--- js/app.js
+++ js/app.js
@@ -41,9 +41,9 @@ var Location = function(data) {
 		self.foursquare_response = resp.response.venues[0];
 		self.address = "<pre>" + self.foursquare_response.location.formattedAddress[0] + "<br>" + self.foursquare_response.location.formattedAddress[1] + "<br>" + self.foursquare_response.location.formattedAddress[2] + "</pre>";
 		self.title = self.foursquare_response.title;
-		self.categories = self.foursquare_response.categories;
+		self.categories = self.foursquare_response.location.categories;
 		console.log(self.categories);
-		$.each(self.foursquare_response.categories,function(i,val) {
+		$.each(self.categories,function(i,val) {
 			if(val.primary === true) {
 				this.main_category = val.name;
 			}
