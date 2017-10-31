1af2c4c035bb7dd12de3e72b506b589a4a3926eb
MarvNet <marvin.kai@gmx.de>
2017-10-31 16:27:06 +0100

    Got the map working!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-mapLocations = [
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="robots" content="index,follow,noarchive">
+	<title>Frankfurt, Germany &raquo; Neighborhood Mao</title>
+	<meta name="description" content="This is a little map with my favourite spots in Frankfurt, Germany.">
+	<meta name="keywords" content="frankfurt,frankfurt am main,frankfurt karte,frankfurt sehenswürdigkeiten">
+	<meta name="creator" content="Marvin S. / https://marvnet.de">
+	<meta name="author" content="Marvin S. / https://marvnet.de">
+</head>
+<body>
+<script src="js/knockout-3.4.2.js"></script>
+<script src="js/jquery-3.2.1.min.js"></script>
+<!--<script src="js/app.js"></script>-->
+<script>
+
+'use strict';
+
+var map;
+
+var mapLocations = [
 	{
 		title: 'MyZeil (Shopping Center)',
 		lat: 50.114354,
@@ -47,10 +68,12 @@ function AppViewModel() {
 
 	this.locations = ko.observableArray([]);
 
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom:11,
+	map = new google.maps.Map(document.getElementsByTagName('body')[0], {
+		zoom:12,
 		center:{lat:50.113929,lng:8.680652}
 	});
+
+	console.log("Intialized map!");
 
 	mapLocations.forEach(function(item) {
 		self.locations.push(new Location(item));
@@ -58,6 +81,8 @@ function AppViewModel() {
 	});
 
 	console.log("Initialized appviewmodel!");
+
+	//this.mapElem.style.height = window.innerHeight - 50;
 
 }
 
@@ -70,3 +95,7 @@ function error() {
 	alert("An error occured!");
 	console.log("An error occured!!");
 }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDf7eLQTk4WpT-FO_ZL7YkUeFcB8QlOLRk&callback=loadEverything&libraries=places" onerror="error()"></script>
+</body>
+</html>
